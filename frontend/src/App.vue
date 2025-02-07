89c39b7d7494f7be3b71e8c9c397cc90215cfbd2
Ismail Shalih Abdul Kholiq <ismailshalih@gmail.com>
2025-02-08 00:59:16 +0700

    display folders

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,74 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, onMounted } from "vue";
+import api from "@/api";
+import FolderComponent from "./components/folderComponent.vue";
+
+const folders = [
+  {
+    id: 1,
+    name: 'home',
+    children: [{
+      id: 2,
+      name: 'child1'
+    }]
+  },
+  {
+    id: 3,
+    name: 'folder1',
+    children: [{
+      id: 4,
+      name: 'child2',
+      children: [
+      {
+        id: 5,
+        name: 'grandchild1'
+      }
+      ]
+    },
+    {
+      id: 6,
+      name: 'child3'
+    }
+  ]
+  }
+]
+
+const currentFolder = ref(null);
+
+const selectFolder = (folder) => {
+  console.log(folder)
+  currentFolder.value = folder;
+};
+
+// const fetchFolders = async () => {
+//   try {
+//     const { data } = await api.get("/folders"); // Calls backend API
+//     folders.value = data;
+//   } catch (error) {
+//     console.error("Error fetching folders:", error);
+//   }
+// };
+
+// onMounted(fetchFolders);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div style="width: 100vw; height:100vh; display: flex;">
+    <div style="width: 30%; padding: 20px;">
+      <h1>Folder Explorer</h1>
+      <div>
+        <div v-for="folder in folders" :key="folder.id">
+          <FolderComponent :folder=folder @select-folder="selectFolder" :onSelectFolder="selectFolder"/>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <div style="width: 70%; padding: 20px;">
+      <h1>{{ currentFolder?.name }}</h1>
+      <div v-if="currentFolder.children.length > 0" style="display: flex;">
+        <div v-for="child in currentFolder.children" :key="currentFolder.children.id" ref="items" style="padding: 8px;">
+            <h2>{{ child.name }}</h2>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ frontend/src/components/FolderComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps(['folder', 'onSelectFolder'])
+const emit = defineEmits(["select-folder"]);
+</script>
+
+<template>
+    <h2 @click="emit('select-folder', props.folder)">{{props.folder.name}}</h2>
+    <div style="margin-left: 20px;">
+        <div v-for="child in props.folder.children" :key="props.folder.children.id" ref="items">
+            <FolderComponent :folder="child" @select-folder="props.onSelectFolder" :onSelectFolder="props.onSelectFolder"/>
+        </div>
+    </div>
+</template>
